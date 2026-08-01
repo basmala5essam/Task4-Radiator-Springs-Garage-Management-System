d2212af2b8cdaf6a580bcbcb760a23b0f68097eb
Basmala Mansour <basmala20.5.2006@gmail.com>
2026-08-02 02:41:35 +0300

    Updated menu and checkIn function

--- task4.cpp
+++ task4.cpp
@@ -1,74 +1,186 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Car
 {
-protected:
+private:
     int car_number, age;
     string fullname, racing_team;
     float speed, capacity;
+
 public:
-void checkIn(Car arr[]){
-    cout << "Enter car number (Note: must be unique)" << endl;
-    cin >> car_number;
-    //validate var number
-    cout << "Enter car fullname" << endl;
-    cin >> fullname;
-    cout << "Enter age (must be positive)" << endl;
-    do{
-        cin >> age;
-        cout << "Invalid age" << endl;
+    void setCarNumber(int carNumber)
+    {
+        car_number = carNumber;
+    }
+    void setAge(int age)
+    {
+        if (age > 0)
+        {
+            this->age = age;
+        }
+        else
+        {
+            cout << "Invalid age" << endl;
+            return;
+        }
+    }
+    void setFullName(string fullName)
+    {
+        fullname = fullName;
+    }
 
-    }while(age<0);
-    cout << "Enter racing team" << endl;
-    cin >> racing_team;
-    cout << "Enter speed (must be positive)" << endl;
-    do{
-        cin >> speed;
-        cout << "Invalid speed" << endl;
+    void setRacingTeam(string RacingTeam)
+    {
+        racing_team = RacingTeam;
+    }
 
-    }while(speed<0);
-    cout << "Enter capacity (must be positive)" << endl;
-    do{
-        cin >> capacity;
-        cout << "Invalid capacity" << endl;
+    void setSpeed(float speed)
+    {
+        if (speed > 0)
+        {
+            this->speed = speed;
+        }
+        else
+        {
+            cout << "Invalid speed" << endl;
+            return;
+        }
+    }
 
-    }while(capacity<0);
+    void setCapacity(float capacity)
+    {
+        if (capacity > 0)
+        {
+            this->capacity = capacity;
+        }
+        else
+        {
+            cout << "Invalid capacity" << endl;
+            return;
+        }
+    }
 
+    int getCarNumber()
+    {
+        return car_number;
+    }
+    int getAge()
+    {
+        return age;
+    }
+    string getFullName()
+    {
+        return fullname;
+    }
+    string getRacingTeam()
+    {
+        return racing_team;
+    }
+    float getSpeed()
+    {
+        return speed;
+    }
+    float getCapacity()
+    {
+        return capacity;
+    }
 
-
-
-}
-virtual float calculatePerformance(){
-
-}
-
+    virtual float calculatePerformance() = 0;
 };
 
 class Racer : public Car
 {
 protected:
     int races, laps;
+
+public:
+    float calculatePerformance() override
+    {
+        float s =getSpeed();
+        float c = getCapacity();
+        float score = (s * 10) + (c * 1);
+        return score;
+    }
 };
 
-class SupportVehicle: public Car
+class SupportVehicle : public Car
 {
 protected:
     int crew_size, reliability_rating;
+public:
+    float calculatePerformance() override
+    {
+        float s =getSpeed();
+        float c = getCapacity();
+        float score = (s * 5) + (c * 5);
+        return score;
+    }
 };
 
+void checkIn(Car *c)
+{
+    int age, carnumber;
+    float speed, capacity;
+    string fullName, racingTeam;
+    cout << "Enter car number (Note: must be unique)" << endl;
+    cin >> carnumber;
+    c->setCarNumber(carnumber);
+    cin.ignore();
+    cout << "Enter car fullname" << endl;
+    getline(cin,fullName);
+    c->setFullName(fullName);
+    cout << "Enter age (must be positive)" << endl;
+    cin >> age;
+    c->setAge(age);
+    cin.ignore();
+    cout << "Enter racing team" << endl;
+    getline(cin, racingTeam);
+    c->setRacingTeam(racingTeam);
+    cout << "Enter speed (must be positive)" << endl;
+    cin >> speed;
+    c->setSpeed(speed);
+    cout << "Enter capacity (must be positive)" << endl;
+    cin >> capacity;
+    c->setCapacity(capacity);
 
+    // calculate Perfomance Score
+    cout << "Car Checked In Successfully!" << endl;
+}
 
-int main(){
+int main()
+{
     int choice;
     cout << "================Garage Management System================" << endl;
-    cout << "1.Check In a Car" << endl << "View the Garage" << endl << "3.Tune-Up (Update a car)" << endl;
-    cout << "4. Retire" << endl << "5.Find a Car" << endl << "6.Garage Report" << endl;
+    cout << "1.Check In a Car" << endl
+         << "2.View the Garage" << endl
+         << "3.Tune-Up (Update a car)" << endl;
+    cout << "4. Retire" << endl
+         << "5.Find a Car" << endl
+         << "6.Garage Report" << endl;
     cin >> choice;
     switch (choice)
     {
     case 1:
-        /* code */
+        Car *c=nullptr;
+        cout << "Enter the type of car (1 for Racer, 2 for Support Vehicle): ";
+        int carType;
+        cin >> carType;
+        if (carType == 1)
+        {
+            c = new Racer();
+        }
+        else if (carType == 2)
+        {
+            c = new SupportVehicle();
+        }
+        else
+        {
+            cout << "Invalid car type" << endl;
+            return 0;
+        }
+        checkIn(c);
         break;
     case 2:
         /* code */
@@ -88,5 +200,4 @@ int main(){
     default:
         break;
     }
-
 }
